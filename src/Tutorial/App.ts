1bc2586e941265b5724f94e848ccfc11606d2952
Taichi Komoto <724komodora915@gmail.com>
2022-05-02 21:26:41 +0900

    add initial tutorial

---
+++ src/Tutorial/App.ts
@@ -0,0 +1,47 @@
+import React from 'react';
+import './index.css';
+
+const Square: React.FC = () => <button className="square">{/* TODO */}</button>;
+
+const Board = () => {
+  const renderSquare = () => <Square />;
+
+  const status = 'Next player: X';
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(2)}
+        {renderSquare(1)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+const Game = () => (
+  <div className="game">
+    <div className="game-board">
+      <Board />
+    </div>
+    <div className="game-info">
+      <div>{/* status */}</div>
+      <ol>{/* TODO */}</ol>
+    </div>
+  </div>
+);
+
+export default function Tutorial() {
+  return <Game />;
+}
